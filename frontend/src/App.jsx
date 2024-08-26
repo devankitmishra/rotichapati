ad5fe6be86953a7e6ff85366b35ddcae8e244032
devankitmishra <ankitmishrapuri@gmail.com>
2024-08-26 23:43:15 +0530

    added navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
