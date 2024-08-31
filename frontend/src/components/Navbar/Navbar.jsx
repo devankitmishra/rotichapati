4ba433871c1042b5c4513156754f2ea6c0c9f1e9
devankitmishra <ankitmishrapuri@gmail.com>
2024-08-31 11:35:22 +0530

    "Updated CSS files for ExploreMenu, Footer, Header, Navbar, and index.css to add media queries and styling changes."

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -11,10 +12,10 @@ const Navbar = () => {
         <img src={assets.logo} alt="" className="logo" />
         {/* //#FFE679  //#280B45 */}
         <ul className='navbar-menu'>
-            <li onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</li>
-            <li onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile app</li>
-            <li onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</li>
+            <Link to='/' onClick={()=>setMenu("home")} className={menu === "home"?"active":""}>home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile app</a>
+            <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact us</a>
         </ul>
         <div className='navbar-right'>
             <img src={assets.search_icon} alt="" />
